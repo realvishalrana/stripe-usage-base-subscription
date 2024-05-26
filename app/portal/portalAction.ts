e6c5cb9587757fe552e9a15cf2266858ce5d89df
Vishal Rana <isvishalrana@gmail.com>
2024-05-26 15:19:40 +0530

    upate env example

--- app/portal/portalAction.ts
+++ app/portal/portalAction.ts
@@ -6,7 +6,7 @@ import { stripe } from "@/utils/stripe";
 export async function createPortalSession(customerId: string) {
     const portalSession = await stripe.billingPortal.sessions.create({
         customer: customerId,
-        return_url: `http://localhost:3000`,
+        return_url: process.env.RETURN_URL,
       });
   
       return { id: portalSession.id, url: portalSession.url };
